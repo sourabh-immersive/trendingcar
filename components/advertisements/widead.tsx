153b9d93dbf5c722aab4f27cb093458a0e69e60e
Sourabh <sourabh@immersiveinfotech.com>
2024-06-10 16:03:31 +0530

    Blog design changes

--- components/advertisements/widead.tsx
+++ components/advertisements/widead.tsx
@@ -15,7 +15,7 @@ const Wide: React.FC<WideProps> = ({ img_url }) => {
     <div className="row ads_wrapper">
       <div className="col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
         <section className="auto-ad-container text-center">
-          <Image className="img-fluid" src={imageUrl} width={968} height={91} alt="trending car" />
+          <Image className="img-fluid" src={imageUrl} width={968} height={91} alt="trending car" priority />
         </section>
       </div>
     </div>
