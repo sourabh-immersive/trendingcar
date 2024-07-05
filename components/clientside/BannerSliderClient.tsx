0d8ba0d67309c72a2351ad6daf14bdb93bdb4f29
atulpawade11 <atul@immersiveinfotech.com>
2024-07-05 16:30:05 +0530

    home page

--- components/clientside/BannerSliderClient.tsx
+++ components/clientside/BannerSliderClient.tsx
@@ -1,89 +1,38 @@
-"use client";
+// components/BannerSlider.js
 
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Autoplay, Navigation } from "swiper/modules";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 
+const slides = [
+  { image: '/blog1.png', text: 'Slide 1' },
+  { image: '/blog2.png', text: 'Slide 2' },
+  { image: '/blog3.png', text: 'Slide 3' }
+];
 
-// Define the shape of the data you expect to receive from the API
-interface Post {
-  id: number;
-  slug: string;
-  title: {
-    rendered: string;
-  };
-  featured_image_url?: string;
-  author?: string; 
-  date?: string; 
-}
-
-
-
-const BannerSliderClient: React.FC<ClientComponentProps> = () => {
-  
-
-//   useEffect(() => {
-//     const getPosts = async () => {
-//       try {
-//         const postsData = await fetchPosts();
-//         setPosts(postsData);
-//       } catch (err: unknown) {
-//         if (err instanceof Error) {
-//           setError(err.message);
-//         } else {
-//           setError(String(err));
-//         }
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     getPosts();
-//   }, []);
-
-//   if (loading) return <LoadingSkeleton />;
-//   if (error) return <p>Error: {error}</p>;
-
-return (
-    <div className="heroAreaMain">
-        <div className="row">
-            <div className="col-md-8 col-sm-12 col-lg-8 col-xl-8 col-xxl-8">
-                <Swiper
-                    spaceBetween={0}
-                    slidesPerView={1}
-                    navigation={true}
-                    // pagination={{ clickable: false }}
-                    scrollbar={{ draggable: true }}
-                    modules={[Autoplay, Navigation]}
-                    // modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-                    className={"BannerSliderClient"}
-                >
-                    {posts.map((post, index) => (
-                    <SwiperSlide data-index={index} key={index}>
-                        <Link href={`/blogs/${post.slug}`} >
-                            <Image
-                            src={
-                                post.featured_image_url ||
-                                "https://via.placeholder.com/600x400"
-                            }
-                            alt={`Slide ${index + 1}`}
-                            width="600"
-                            height="400"
-                            />
-                            <h2>{post.title.rendered}</h2>
-                        </Link>
-                    </SwiperSlide>
-                ))}
-                </Swiper>
+const BannerSlider = () => {
+  return (
+    <div className="slider">
+      <Swiper
+        modules={[Autoplay, Navigation, Pagination]}
+        loop={true}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div className="image" style={{ backgroundImage: `url(${slide.image})`, height: '600px', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+              <div className="text">{slide.text}</div>
             </div>
-        </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
 
-export default BannerSliderClient;
+export default BannerSlider;
--- components/clientside/home/HomeContent.tsx
+++ components/clientside/home/HomeContent.tsx
@@ -19,18 +19,17 @@ const HomeContent = ({ params }: { params: { slug: string } }) => {
             <div className="row mt-4">
                 <div className="col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
                     <section className="banner-section">
-                        <div className="container">
+                        <BannerSliderClient />
+                        <div className="wraps">
                             <div className="row">
                                 <div className="col-md-6 offset-md-3">
-                                    <div className="slider">
-                                        <BannerSliderClient />
-                                    </div>
-                                    
-                                    <h6>TRUSTED CAR DEALER SERVICES</h6>
-                                    <h1>FIND THE RIGHT CAR</h1>
-                                    <div className="form-group pos-rel">
-                                        <input type="text" className="form-control home-search-input" id="search" placeholder="Type to select car name" />
-                                        <button type="button" className="btn btn-primary pos-abs home-search-btn">Search Car</button>
+                                    <div className="container">
+                                        <h6>TRUSTED CAR DEALER SERVICES</h6>
+                                        <h1>FIND THE RIGHT CAR</h1>
+                                        <div className="form-group pos-rel">
+                                            <input type="text" className="form-control home-search-input" id="search" placeholder="Type to select car name" />
+                                            <button type="button" className="btn btn-primary pos-abs home-search-btn">Search Car</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
