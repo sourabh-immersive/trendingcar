60335e3dfedec79d3936db49cabfbd35d63c66d4
atulpawade11 <atul@immersiveinfotech.com>
2024-07-12 18:37:42 +0530

    css changes

--- components/clientside/home/CompareCars.tsx
+++ components/clientside/home/CompareCars.tsx
@@ -4,6 +4,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import Image from 'next/image'
 
 const comparisonsWrapper = [
   {
@@ -50,47 +51,56 @@ const CompareCars = () => {
               <h5 className="page-title">Explore All vechicles</h5>
             </div>
             <div className="slider">
-              <Swiper
-              modules={[Autoplay, Navigation]}
-              loop={true}
-              autoplay={{ delay: 3000, disableOnInteraction: false }}
-              navigation
-              slidesPerView={4}
-              spaceBetween={0}
-              >
-              {comparisonsWrapper.map((car, index) => (
-                  <SwiperSlide key={index}>
-                    <div className="comparisonsWrapper shadowWPadding clearfix brandModel">
-                        <div className="carBoxs" data-img-fit="true">
-                            <div className="gsc_col-xs-12 paddingnone hover">
-                                <div className="vsTag">VS</div>
-                                <img className="hover imageTransition active" data-autofit="true" decoding="async" title="FRONX vs Taisor" alt="FRONX vs Taisor" src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Maruti/FRONX/9243/1697697928533/front-left-side-47.jpg " loading="eager" data-lazy="true" />
-                            </div>
-                            <div className="gsc_col-xs-12 holder">
-                                <span className="undefined brandTitle truncate">Maruti</span>
-                                <span className="undefined modelTitle truncate">FRONX</span>
-                                <div className="price">
-                                    <span className="icon-cd_R">Rs.</span>7.51 - 13.04 Lakh<sup> *</sup>
+                <Swiper
+                modules={[Autoplay, Navigation]}
+                loop={true}
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                navigation
+                slidesPerView={4}
+                spaceBetween={0}
+                >
+                {comparisonsWrapper.map((car, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="comparison-wrapper">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="img-wrapper">
+                                        <Image
+                                        src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Maruti/FRONX/9243/1697697928533/front-left-side-47.jpg"
+                                        width={500}
+                                        height={500}
+                                        alt="Picture of the author"
+                                        />
+                                    </div>
+                                    <div className="content-wrapper">
+                                        <span className="brandTitle truncate">Maruti</span>
+                                        <span className="modelTitle truncate">FRONX</span>
+                                        <div className="price">
+                                            <span className="icon-cd_R">Rs.</span>7.51 - 13.04 Lakh<sup> *</sup>
+                                        </div>
+                                    </div>
+                                    <div className="vsTag">VS</div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="img-wrapper">
+                                        <img className="hover imageTransition active" data-autofit="true" decoding="async" title="FRONX vs Taisor" alt="FRONX vs Taisor" src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Toyota/Urban-Cruiser-Taisor/11639/1712131241368/front-left-side-47.jpg" loading="eager" data-lazy="true" />
+                                    </div>
+                                    <div className="content-wrapper">
+                                        <span className="brandTitle truncate">Maruti</span>
+                                        <span className="modelTitle truncate">FRONX</span>
+                                        <div className="price">
+                                            <span className="icon-cd_R">Rs.</span>7.51 - 13.04 Lakh<sup> *</sup>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-12">
+                                    <a href="#" className=" primaryButton" title="FRONX vs Taisor" data-lt="/compare/maruti-fronx-and-toyota-taisor.htm">
+                                        Maruti FRONX vs Toyota Taisor
+                                    </a>
                                 </div>
                             </div>
                         </div>
-                        <div className="carBoxs" data-img-fit="true">
-                            <div className="gsc_col-xs-12 hover paddingnone">
-                                <img className="hover imageTransition active" data-autofit="true" decoding="async" title="FRONX vs Taisor" alt="FRONX vs Taisor" src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Toyota/Urban-Cruiser-Taisor/11639/1712131241368/front-left-side-47.jpg" loading="eager" data-lazy="true" /></div>
-                                <div className="gsc_col-xs-12 holder ">
-                                    <span className="undefined brandTitle truncate">Toyota</span>
-                                    <span className="undefined modelTitle truncate">Taisor</span>
-                                    <div className="price"><span className="icon-cd_R">Rs.</span>7.74 - 13.04 Lakh<sup> *</sup>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="buttonHolder clear">
-                            <a href="/compare/maruti-fronx-and-toyota-taisor.htm" className=" primaryButton" title="FRONX vs Taisor" data-lt="/compare/maruti-fronx-and-toyota-taisor.htm">Maruti FRONX vs Toyota Taisor</a>
-                        </div>
-                    </div>
-
-                  
-                  </SwiperSlide>
+                   </SwiperSlide>
               ))}
               </Swiper>
             </div>
